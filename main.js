f60cdb23db80044d93dbc8f9eb593f32899c093f
Ehsan Davoudi <ehsan.davoudi.1@gmail.com>
2024-04-16 00:28:03 +0330

    add a onmousemove to move the rocket

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let rocket = document.getElementById("rocket");
 
 window.onload = function () {
   setInterval(changeRocketAssets, 100);
+  window.onmousemove = function (e) {
+    if (e.clientX <= window.innerWidth - 100) {
+      rocket.style.left = e.clientX + "px";
+    }
+    if (e.clientY <= window.innerHeight - 180) {
+      rocket.style.top = e.clientY + "px";
+    }
+  };
 };
 
 function changeRocketAssets() {
